206751d05d4e20b4d5b6a995458fcbaabae206b6
Jason Meulenhoff <j.meulenhoff@me.com>
2025-05-27 11:12:03 +0200

    Initial project structure

---
+++ Events/ActionDisposable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FrameWork.Events.Input
+{
+    public class ActionDisposable : IDisposable
+    {
+        private Action disposeAction;
+
+        public ActionDisposable(Action dispose)
+        {
+            disposeAction = dispose;
+        }
+
+        public void Dispose()
+        {
+            disposeAction?.Invoke();
+            disposeAction = null;
+        }
+    }
+}
---
+++ Events/ControlEvent.cs
@@ -0,0 +1,10 @@
+﻿using Framework.Events.Input;
+
+namespace Core.Components.Input
+{
+    public struct ControlEvent<T>
+    {
+        public InputState state;
+        public T value;
+    }
+}
